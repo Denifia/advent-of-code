d7e05d48740f77f16e8b9874b9ef0d96e98c5179
Luke Wale <luke.wale@cashconverters.com>
2025-12-05 14:34:30 +0800

    2025-02 part 1 done

---
+++ 2025/02/app.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Immutable;
+
+var ranges = File.ReadAllText("input.txt")
+                       .Split(',')
+                       .Select(range => new Range(range))
+                       .ToImmutableArray();
+
+long sum = 0;
+foreach (var range in ranges)
+{
+    sum += range.SumInvalidItems();
+}
+
+Console.WriteLine($"Invalid ID sum: {sum}");
+
+
+class Range
+{
+    public long Start { get; }
+    public long End { get; }
+
+    public Range(string range)
+    {
+        var r = range.AsSpan();
+        var split = r.IndexOf('-');
+        Start = long.Parse(r[..split]);
+        End = long.Parse(r[(split + 1)..]);
+    }
+
+    public long SumInvalidItems()
+    {
+        long sum = 0;
+        for (long i = Start; i <= End; i++)
+        {
+            var number = i.ToString().AsSpan();
+
+            // skip odd length numbers
+            if (number.Length % 2 == 1)
+            {
+                continue;
+            }
+
+            int halfLength = number.Length / 2;
+            if (number[..halfLength].SequenceEqual(number[halfLength..]))
+            {
+                sum += i;
+            }
+        }
+        return sum;
+    }
+}
